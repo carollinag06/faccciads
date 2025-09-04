80f99fea7df44b9e9a2acb706b5c24d747557de7
carollinag06 <carollinag06@gmail.com>
2025-09-03 21:51:32 -0300

    Update exercicios_objetos.js

--- RevisaoJsAula01-main/RevisaoJsAula01-main/exercicios_objetos.js
+++ RevisaoJsAula01-main/RevisaoJsAula01-main/exercicios_objetos.js
@@ -5,12 +5,43 @@ Exercícios Práticos - Objetos
 */
 
 // 1. Crie um objeto com 3 propriedades diferentes e exiba-o.
+let pessoa1={
+    nome: 'ana',
+    idade: 20,
+    sexo: 'f'
+}
+
+console.log(pessoa1)
 
 // 2. Acesse e exiba uma propriedade usando notação de ponto e colchetes.
+let pessoa2={
+    nome: 'pedro',
+    idade: 87,
+    sexo: 'm'
+}
+
+console.log(pessoa2(sexo))
 
 // 3. Adicione uma nova propriedade ao objeto depois de criado.
+let pessoa3={
+    nome: 'pedro',
+    idade: 87,
+    sexo: 'm'
+}
+pessoa3.profissao='medico'
+
+console.log(pessoa3)
 
 // 4. Remova uma propriedade do objeto.
+let pessoa4={
+    nome: 'pedro',
+    idade: 87,
+    sexo: 'm'
+}
+
+
+
+console.log(pessoa4(sexo))
 
 // 5. Adicione um método que retorne uma string usando propriedades do objeto.
 
